0d9dca7c3cb5c6c707bd91dbe3c16d808616a1a4
hervez <herve.zumbach@gmail.com>
2023-10-27 15:13:35 +0200

    main file finished with all scripts

--- main.R
+++ main.R
@@ -1,2 +1,5 @@
 source("scripts/0-load-data.R", echo=TRUE)
 source("scripts/1-preliminary-analysis.R", echo=TRUE)
+source("scripts/2-political-analysis.R", echo=TRUE)
+source("scripts/3-modelling.R", echo=TRUE)
+
